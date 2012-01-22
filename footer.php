76ca0f8d31ecf0eeeae402fba819b22aa347b402
ArmNo <monkeyarmno@gmail.com>
2012-01-22 23:09:53 +0700

    added page.php for page content. added more links in footer.

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 		</div><!-- .main -->
 		<footer>
-			<p>&copy; <?php echo date('Y'); ?> &mdash; <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-			icons by <a href="http://picons.me/">Picons Social</a> and <a href="http://pc.de/icons/">pc.de</a></p>
+			<p>
+				<strong><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></strong> is powered by <a href="http://wordpress.org">WordPress</a>. Icons by <a href="http://picons.me/">Picons Social</a> and <a href="http://pc.de/icons/">pc.de</a>. <a href="https://github.com/armno/Sera-WordPress-Theme">Sera Theme</a> (alpha) by ArmNo.
+			</p>
 		</footer>
 
 	</body>
---
+++ page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<div class="content single">
+			<article <?php post_class(); ?>>
+				<h2 class="post-title"><?php the_title(); ?></h2>
+				
+				<?php the_content(); ?>
+			</article>
+	</div><!-- .content -->
+<?php endwhile;
+	else : ?>
+	<p>Sorry, no pages found.</p>
+	<?php endif; ?>
+<?php get_footer(); ?>
