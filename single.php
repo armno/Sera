432c3dd50a8adb04b3ceea5db0af12433369db7e
ArmNo <arm@snapplabs.com>
2012-01-25 13:29:00 +0700

    moved sharing buttons to the end of the post

--- single.php
+++ single.php
@@ -10,23 +10,6 @@
 			<li><?php the_tags('Tags<div>','&nbsp;&nbsp;', '</div>'); ?></li>
 		</ul>
 
- 		<!-- <section class="social-buttons clearfix">
-
-			<div id="fb-root"></div>
-			<a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php the_title(); ?>" data-url="<?php the_permalink(); ?>" data-count="horizontal" data-via="armno" data-related="armno">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
-
-			<script>(function(d, s, id) {
-			  var js, fjs = d.getElementsByTagName(s)[0];
-			  if (d.getElementById(id)) {return;}
-			  js = d.createElement(s); js.id = id;
-			  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-			  fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));</script>
-
-			<div class="fb-like" data-send="false" data-width="60" data-show-faces="true" data-layout="button_count"></div>
-
-        </section> -->
-
         <p id="jump">
 	        <a href="#main">back to top</a>
         </p>
@@ -38,7 +21,25 @@
 				
 				<?php the_content(); ?>
 
-				<div class="related-posts">
+				<section class="social-buttons clearfix">
+					<h4>Share</h4>
+					<div id="fb-root"></div>
+
+					<script>(function(d, s, id) {
+					  var js, fjs = d.getElementsByTagName(s)[0];
+					  if (d.getElementById(id)) {return;}
+					  js = d.createElement(s); js.id = id;
+					  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+					  fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>
+
+					<div class="fb-like" data-send="false" data-width="40" data-show-faces="true" data-layout="button_count"></div>
+
+					<a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php the_title(); ?>" data-url="<?php the_permalink(); ?>" data-count="horizontal" data-via="armno" data-related="armno">Tweet</a><script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
+
+		        </section>
+
+		        <section class="related-posts">
 					<?php
 					    $tags = get_the_tags();
 
@@ -70,34 +71,8 @@
 
 					    endforeach;
 					    wp_reset_query();
-					    /*
-					    if ($tags) :
-					        $first_tag = $tags[0]->term_id;
-					        $args = array(
-							    'tag__in' => array($first_tag),
-							    'post__not_in' => array($post->ID),
-							    'showposts'=>5,
-							    'ignore_sticky_posts'=>1
-							);
-
-						$my_query = new WP_Query($args);
-
-						if( $my_query->have_posts() ) : ?>
-						    <h4>Related Posts (beta)</h4>
-						    <ul>
-						    <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-						    	<li>
-							    <a href="<?php the_permalink() ?>" rel="bookmark" title="
-							    Permanent Link to <?php the_title_attribute(); ?>">
-						            <?php the_title(); ?></a>
-						        </li>
-						    <?php endwhile;  ?>
-						    </ul>
-					<?php  endif;
-					    endif;
-					    wp_reset_query(); */
 					?>
-					</div>
+				</section>
 			</article>
 			<?php comments_template(); ?> 
 	</div><!-- .content -->
