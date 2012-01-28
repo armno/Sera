76b15d1e801db71be2a4ebc7e8cf38222156dec4
ArmNo <monkeyarmno@gmail.com>
2012-01-28 16:12:04 +0700

    separate comments and trackbacks in comment callback function

--- comments.php
+++ comments.php
@@ -1,10 +1,10 @@
 <div class="comments">
 <?php if ( have_comments() ) : ?>
-	<h3><?php comments_number('No Comments', 'One Comment', '% Comments' );?></h3>
+	<h3><?php comments_number('No Comments', '1 Comment', '% Comments' );?></h3>
 
 	<!-- View functions.php for comment markup -->
 	<ol class="comments-list">
-		<?php wp_list_comments('type=comment&callback=sera_comments'); ?>
+		<?php wp_list_comments('callback=sera_comments'); ?>
 	</ol>
 
 	<?php previous_comments_link() ?> <?php next_comments_link() ?>
--- functions.php
+++ functions.php
@@ -9,19 +9,37 @@ if ( function_exists('register_sidebar') ) {
 }
 
 function sera_comments( $comment, $args, $depth ) {
-	$GLOBALS['comment'] = $comment; ?>
-	<li <?php comment_class(); ?> id="comment-<?php comment_ID() ?>">
-		<div class="avatar">
-			<?php echo get_avatar( $comment->comment_author_email, 64 ); ?>
-		</div>
-		<div class="comment-text">
-			<h4><?php echo get_comment_author_link(); ?></h4>
-			<p class="comment-time"><?php echo get_comment_date(); ?></p>
-			<?php comment_text() ?>
-		</div>
-		<div class="reply">
-         	<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-        </div>
+	$GLOBALS['comment'] = $comment;
+
+	switch ( $comment->comment_type ) :
+	 	case '':
+	 		?>
+			<li <?php comment_class(); ?> id="comment-<?php comment_ID() ?>">
+				<div class="avatar">
+					<?php echo get_avatar( $comment->comment_author_email, 64 ); ?>
+				</div>
+				<div class="comment-text">
+					<h4><?php echo get_comment_author_link(); ?></h4>
+					<p class="comment-time"><?php echo get_comment_date(); ?></p>
+					<?php comment_text() ?>
+				</div>
+				<div class="reply">
+		         	<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+		        </div>
+	 		<?php
+	 		break;
+	 	
+	 	case 'trackback':
+	 	case 'pingback' : 
+	 		?>
+	 		<li <?php comment_class(); ?> id="comment-<?php comment_ID() ?>">
+	 			<span>Trackback: </span>
+				<?php echo get_comment_author_link(); ?>
+				<span class="comment-time"> &mdash; <?php echo get_comment_date(); ?></span>
+	 		<?php
+	 		break;
+	endswitch; ?>
+	
 	<?php
 }
 
