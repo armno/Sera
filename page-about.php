5e0af84c55e1890e19963fe0623d15919c3bcab4
ArmNo <arm@snapplabs.com>
2012-02-17 17:01:59 +0700

    fixed content width of about page

--- page-about.php
+++ page-about.php
@@ -1,8 +1,14 @@
+<?php 
+/*
+Template Name: About
+*/
+?>
+
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="content single">
+	<div class="content single page-about">
 			<article <?php post_class(); ?>>
-				<!-- <h2 class="post-title"><?php the_title(); ?></h2> -->
+				<h2 class="post-title"><?php the_title(); ?></h2>
 				
 				<?php the_content(); ?>
 			</article>
