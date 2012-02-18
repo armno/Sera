077e258a3c327a6e11d4986af38912a47d4459d4
ArmNo <arm@snapplabs.com>
2012-02-18 10:04:12 +0700

    align footer text to center. increased content padding

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 		</div><!-- #main -->
 		<footer>
 			<p>
-				<strong><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></strong> is powered by <a href="http://wordpress.org">WordPress</a>. Icons by <a href="http://picons.me/">Picons Social</a> and <a href="http://pc.de/icons/">pc.de</a>. <a href="https://github.com/armno/Sera-WordPress-Theme">Sera Theme</a> (alpha) by ArmNo.
+				<strong><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></strong> is powered by <a href="http://wordpress.org">WordPress</a>.
+				Icons by <a href="http://picons.me/">Picons Social</a> and <a href="http://pc.de/icons/">pc.de</a>. <a href="https://github.com/armno/Sera-WordPress-Theme"><strong>Sera Theme</strong></a> (alpha) by ArmNo.
 			</p>
 		</footer>
 		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
