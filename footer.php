7fb594d16f73047e603967bd1f9856a257c948e6
ArmNo <arm@snapplabs.com>
2012-02-18 15:26:02 +0700

    align center for footer text

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 		</div><!-- #main -->
 		<footer>
 			<p>
-				<strong><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></strong> is powered by <a href="http://wordpress.org">WordPress</a>.
-				Icons by <a href="http://picons.me/">Picons Social</a> and <a href="http://pc.de/icons/">pc.de</a>. <a href="https://github.com/armno/Sera-WordPress-Theme"><strong>Sera Theme</strong></a> (alpha) by ArmNo.
+				<strong><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></strong> is powered by <a href="http://wordpress.org">WordPress</a> and <a href="https://github.com/armno/Sera-WordPress-Theme">Sera Theme</a>.
+				Icons by <a href="http://picons.me/">Picons Social</a> and <a href="http://pc.de/icons/">pc.de</a>.
 			</p>
+			<p>Made in Chiang Mai.</p>
 		</footer>
 		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 	</body>
