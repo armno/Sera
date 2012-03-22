7acb92febfcea43306b2dab6531ad81663382987
ArmNo <monkeyarmno@gmail.com>
2012-03-22 21:29:13 +0700

    added social medias options in theme options page

--- theme-options.php
+++ theme-options.php
@@ -1,6 +1,21 @@
 <?php
 // Options Page Functions
 
+if ( isset($_POST['social-media']) ) {
+	sera_save_data($_POST);
+	?>
+	<div id="setting-error-settings_updated" class="updated settings-error"> 
+	<p><strong>Settings saved.</strong></p></div>
+	<?php
+}
+
+function sera_save_data( $data ) {
+    update_option('sera_facebook_url', $data['facebook-page']);
+    update_option('sera_twitter_username', $data['twitter-username']);
+    update_option('sera_flickr_username', $data['flickr-username']);
+    update_option('sera_feeds_url', $data['rss-url']);
+}
+
 function themeoptions_admin_menu()
 {
 	// here's where we add our theme options page link to the dashboard sidebar
@@ -11,7 +26,7 @@ function themeoptions_page()
 {
 	// here is the main function that will generate our options page
 	?>
-	<div class="wrap">
+	<div class="wrap sera-options">
 		<div id="icon-themes" class="icon32"><br></div>
 		<h2>Sera Theme Options</h2>
 		<p>
@@ -25,7 +40,51 @@ function themeoptions_page()
 			<li>Main Menu Icons</li>
 			<li>Blog Logo</li>
 		</ul>
+
+		<h3>Social Medias</h3>
+		<form action="admin.php?page=theme-options.php" method="post">
+			<input type="hidden" name="social-media" value="1">
+			
+			<div class="form-input">
+				<label for="facebook-page">Facebook Page URL</label>
+				<input type="text" name="facebook-page" id="facebook-page" placeholder="e.g. http://www.facebook.com/pagename" value="<?php echo get_option('sera_facebook_url'); ?>">
+			</div>
+
+			<div class="form-input">
+				<label for="twitter-username">Twitter Username</label>
+				<input type="text" name="twitter-username" id="twitter-username" placeholder="e.g. armno (without @)" value="<?php echo get_option('sera_twitter_username'); ?>">
+			</div>
+
+			<div class="form-input">
+				<label for="flickr-username">Flickr Username</label>
+				<input type="text" name="flickr-username" id="flickr-username" placeholder="e.g. armno" value="<?php echo get_option('sera_flickr_username'); ?>">
+			</div>
+
+			<div class="form-input">
+				<label for="rss-url">RSS Feed URL</label>
+				<input type="text" name="rss-url" id="rss-url" placeholder="e.g. http://www.yourblog.com/feed" value="<?php echo get_option('sera_feeds_url'); ?>">
+			</div>
+			<input type="submit" value="Submit" class="button-primary">
+		</form>
 	</div>
+
+	<style type="text/css">
+		/*  =================
+		    Theme Options
+		    ============== */
+		.sera-options label {
+		    display: inline-block;
+		    min-width: 150px;
+		}
+
+		.sera-options .form-input {
+			margin: 10px 0;
+		}
+
+		.sera-options .form-input input[type=text] {
+			width: 300px;
+		}
+	</style>
 	
 	<?php
 }
