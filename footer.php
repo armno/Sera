c3c4b3d68ac27478e430f292b3a7c76d29b8b355
ArmNo <monkeyarmno@gmail.com>
2012-05-20 11:44:33 +0700

    remove footer links of icon sets

--- footer.php
+++ footer.php
@@ -2,36 +2,9 @@
 		<footer>
 			<p>
 				<strong><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></strong> is powered by <a href="http://wordpress.org">WordPress</a> and <a href="https://github.com/armno/Sera-WordPress-Theme">Sera Theme</a>.
-				Icons by <a href="http://picons.me/">Picons Social</a> and <a href="http://pc.de/icons/">pc.de</a>.
 			</p>
 			<p>Made in Chiang Mai.</p>
 		</footer>
-		<?php /*
-		<script>
-			// inline small script to reduce number of HTTP requests
-			jQuery(document).ready(function($) {
-
-				// source: http://webdesignerwall.com/tutorials/animated-scroll-to-top
-				// hide/show the jump to top link
-				$(window).scroll(function() {
-					if ( $(this).scrollTop() > 100 ) {
-						$('#jump').fadeIn();
-					} else {
-						$('#jump').fadeOut();
-					}
-				});
-
-				// add scrolling to top of the page animation
-				$('#jump').click(function() {
-					$('body, html').animate({
-						scrollTop: 0
-					}, 800);
-					return false;
-				});	
-
-			});
-		</script>
-		*/ ?>
 		<?php wp_footer(); ?>
 	</body>
 </html>
