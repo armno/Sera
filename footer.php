45d3760f0a144a9d2e44a00e6632c615f32b0dbf
ArmNo <monkeyarmno@gmail.com>
2012-09-24 21:31:11 +0700

    added dummy commit messages in footer

--- footer.php
+++ footer.php
@@ -3,7 +3,24 @@
 			<p>
 				<strong><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></strong> is powered by <a href="http://wordpress.org">WordPress</a> and <a href="https://github.com/armno/sera">Sera Theme</a>.
 			</p>
-			<p>Made in Chiang Mai.</p>
+			<!-- <p>Made in Chiang Mai.</p> -->
+
+			<section class="commits">
+				<h4>Latest commits</h4>
+				<ul style="font-family: 'Droid Sans Mono'; font-size: 14px; text-shadow: 0 1px 0 #141414;">
+					<li><a href="#">94d4284</a> (HEAD, origin/master, origin/HEAD, master) fixed links:hover color in header</li>
+					<li><a href="#">5df53ad</a> fixed responsive header</li>
+					<li><a href="#">8b11beb</a> close #8. remove some login page styles.</li>
+					<li><a href="#">0b06038</a> fixed viewport</li>
+					<li><a href="#">922309a</a> fixed header width for < 980px window</li>
+					<li><a href="#">5c9e724</a> added editor style fixed #5</li>
+					<li><a href="#">857ed60</a> added states for social icons</li>
+					<li><a href="#">fa2c696</a> removed deprecated functions</li>
+					<li><a href="#">65ad39c</a> change page width and margins.</li>
+					<li><a href="#">a5e4499</a> update readme</li>
+				</ul>
+			</section>
+			
 		</footer>
 		<?php wp_footer(); ?>
 	</body>
