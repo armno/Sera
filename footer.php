9cc4956c78742de0dc8eeb826fc0a020a0fce696
ArmNo <arm@snapplabs.com>
2012-10-02 18:00:02 +0700

    disable shit displaying for all touch devices in footer

--- footer.php
+++ footer.php
@@ -12,38 +12,40 @@
 			</section>
 			<script>
 				jQuery(function($){
-					$.getJSON('https://api.github.com/repos/armno/Sera/commits',
-						function(commits) {
-							var fragment = document.createDocumentFragment(),
-								i = 0, 
-								l = commits.length,
-								ul = document.getElementById('commits-list'),
-								a,
-								li,
-								sha,
-								msg,
-								href,
-								target;
+					// disable commits displaying for all touch devices
+					if ( Modernizr.touch ) {
+						var commitSection = document.querySelector('.commits');
+						commitSection.parentNode.removeChild(commitSection);
+					} else {
+						$.getJSON('https://api.github.com/repos/armno/Sera/commits',
+							function(commits) {
+								var fragment = document.createDocumentFragment(),
+									i = 0, 
+									l = commits.length,
+									ul = document.getElementById('commits-list'),
+									a,
+									li,
+									msg;
 
-							for ( ; i < l; i++ ) {
+								for ( ; i < l; i++ ) {
 
-								a = document.createElement('a');
-								a.setAttribute('href', 'https://github.com/armno/Sera/commit/' + commits[i].sha);
-								a.setAttribute('target', '_blank');
-								a.textContent = commits[i].sha.substring(0,7);
+									a = document.createElement('a');
+									a.setAttribute('href', 'https://github.com/armno/Sera/commit/' + commits[i].sha);
+									a.setAttribute('target', '_blank');
+									a.textContent = commits[i].sha.substring(0,7);
 
-								msg = document.createTextNode(' ' + commits[i].commit.message);
-								
-								li = document.createElement('li');
-								li.appendChild(a);
-								li.appendChild(msg);
+									msg = document.createTextNode(' ' + commits[i].commit.message);
+									
+									li = document.createElement('li');
+									li.appendChild(a);
+									li.appendChild(msg);
 
-								fragment.appendChild(li);
-							}
+									fragment.appendChild(li);
+								}
 
-							ul.appendChild(fragment);
-						});
-
+								ul.appendChild(fragment);
+							});
+					}
 				});
 			</script>
 		</footer>
--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html class="no-js" <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
@@ -13,6 +13,7 @@
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 		<?php if ( is_singular() && get_option('thread_comments') ) wp_enqueue_script('comment-reply'); ?>
+		<?php wp_enqueue_script('moderinzr', get_template_directory_uri() . '/js/modernizr.js'); ?>
 		<?php wp_enqueue_script('jquery'); ?>
 		<?php wp_head(); ?>
 	</head>
