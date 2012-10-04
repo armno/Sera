475de7f3f7097298cade13b92d37fa0ac17fd990
ArmNo <arm@snapplabs.com>
2012-10-04 21:07:58 +0700

    closed #15. added facebook like box in single post view

--- single.php
+++ single.php
@@ -102,6 +102,18 @@
 					?>
 				</section>
 
+				<section class="like-box">
+					<div id="fb-root"></div>
+					<script>(function(d, s, id) {
+					  var js, fjs = d.getElementsByTagName(s)[0];
+					  if (d.getElementById(id)) return;
+					  js = d.createElement(s); js.id = id;
+					  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=183426421717643";
+					  fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>
+					<div class="fb-like-box" data-href="http://www.facebook.com/armnoblog" data-width="874" data-show-faces="true" data-stream="false" data-header="false"></div>
+				</section>
+
 			</article>
 			<?php comments_template(); ?> 
 	</div><!-- .content -->
