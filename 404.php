d316f838b747be9638ae16f2c0960bd1a2e0584a
ArmNo <arm@snapplabs.com>
2012-12-14 19:27:12 +0700

    created a 404 page with shitty inline styles

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div style="text-align: center; font-size: 14em; line-height: 1;">
+    ಠ_ಠ
+</div>
+
+<p style="text-align: center; margin: 120px 0 80px; font-size: 2em;">not found</p>
+
+<p style="text-align: center;"><a href="<?php echo home_url(); ?>">back to home page</a></p>
+<div class="logo logo-post" style="font-size: 1em; text-align: center; margin-top: 0;">
+    <a href="<?php echo home_url(); ?>">AN</a>
+</div>
+<?php get_footer(); ?>
